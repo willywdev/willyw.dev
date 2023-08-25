24b54d4035656c5dd846ecc8a6f6261153dc3cc3
Wilhelm Werwein <werwein.dev@gmail.com>
2023-08-25 13:02:21 +0200

    change structure to Header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,17 +3,19 @@ import "./Header.css";
 export default function Header() {
   return (
     <header className="header">
-      <img className="header__logo" src="/IMG_0365.jpg" alt="logo" />
-      <article>
-        <h1 className="header__headline">willyw.dev</h1>
-        <p>Software Developer</p>
-        <p>Full Stack Developer</p>
-      </article>
-      <article className="header__contact-links">
+      <section className="header__main-content">
+        <img className="header__logo" src="/IMG_0365.jpg" alt="logo" />
+        <article>
+          <h1 className="header__headline">willyw.dev</h1>
+          <p>Software Developer</p>
+          <p>Full Stack Developer</p>
+        </article>
+      </section>
+      <section className="header__contact-links">
         <a>Email</a>
         <a>Github</a>
         <a>LinkedIn</a>
-      </article>
+      </section>
     </header>
   );
 }
