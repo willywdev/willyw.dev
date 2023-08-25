4702ac9651b7bf8e8c12e78d7f6906471c5d4d62
Wilhelm Werwein <werwein.dev@gmail.com>
2023-08-25 12:45:44 +0200

    add Header component content

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,3 +1,19 @@
+import "./Header.css";
+
 export default function Header() {
-  return <h1>willyw.dev</h1>;
+  return (
+    <header className="header">
+      <img className="header__logo" src="/IMG_0365.jpg" alt="logo" />
+      <article>
+        <h1 className="header__headline">willyw.dev</h1>
+        <p>Software Developer</p>
+        <p>Full Stack Developer</p>
+      </article>
+      <article className="header__contact-links">
+        <a>Email</a>
+        <a>Github</a>
+        <a>LinkedIn</a>
+      </article>
+    </header>
+  );
 }
