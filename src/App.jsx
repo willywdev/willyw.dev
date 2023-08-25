6ee8107d0fbbe913762925a8f90c8f0ccb9357c6
Wilhelm Werwein <werwein.dev@gmail.com>
2023-08-25 13:45:07 +0200

    create + add content Footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
+import Footer from "./components/Footer/Footer";
 import Header from "./components/Header/Header";
-
 function App() {
-  return <Header />;
+  return (
+    <>
+      <Header />
+      <Footer />
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,9 @@
+import "./Footer.css";
+
+export default function Footer() {
+  return (
+    <footer className="footer">
+      <p className="footer__text">Created with ❤️ and React</p>
+    </footer>
+  );
+}
