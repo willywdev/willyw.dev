2dce54ff93ea4e99eddde6770b956df0c044458c
Wilhelm Werwein <werwein.dev@gmail.com>
2023-08-26 11:33:27 +0200

    add Skill Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import Footer from "./components/Footer/Footer";
 import Header from "./components/Header/Header";
+import Skills from "./components/Skills/Skills";
 function App() {
   return (
     <>
       <Header />
+      <Skills />
       <Footer />
     </>
   );
