e40e7bb20ad5d44b9dba3ee1bf39c6f4d6444c94
Wilhelm Werwein <werwein.dev@gmail.com>
2023-08-28 13:04:52 +0200

    change styling of Projects component

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -6,15 +6,24 @@ export default function Projects() {
     <section className="projects">
       <h2>Relevant Projects</h2>
       {projectsList.map((project) => (
-        <a href={project.url} key={project.id} className="projects__wrapper">
+        <a
+          href={project.url}
+          key={project.id}
+          className="projects__wrapper"
+          style={{ backgroundColor: project.backgroundColor }}>
           <img
             src={project.img}
             alt={project.name}
             className="projects__logo"
           />
           <article className="projects__project-text">
-            <h3 className="projects__headline">{project.name}</h3>
-            <p>{project.description}</p>
+            <h3 className="projects__headline" style={{ color: project.color }}>
+              {project.name}
+            </h3>
+            <p className="projects__description">{project.description}</p>
+            <button type="button" className="projects__repo-button">
+              See code
+            </button>
           </article>
         </a>
       ))}
--- src/components/Projects/projectsList.js
+++ src/components/Projects/projectsList.js
@@ -2,26 +2,23 @@ const projectsList = [
   {
     name: "Linkster",
     url: "",
-    color: "lightpink",
-    description: "ksadmlsakdmalskdasdklas",
-    img: "",
+    color: "rgb(200, 228, 178)",
+    backgroundColor: "rgba(200, 228, 178, 12%)",
+    description:
+      "A Linktree clone with my personal links. To see more projects of mine, click here.",
+    img: "./icons8-linktree.svg",
+    repo: "",
     id: 1,
   },
   {
     name: "React Skill Icons",
     url: "",
-    color: "lightblue",
-    description: "ksadmlsakdmalskdasdklas",
-    img: "",
+    color: "rgb(161, 204, 209)",
+    backgroundColor: "rgba(161, 204, 209, 12%)",
+    description: "NPM Package to use Skill Icons by tandpfun in React.",
+    img: "./icons8-react-native.svg",
+    repo: "",
     id: 2,
-  },
-  {
-    name: "Project 3",
-    url: "",
-    color: "lightred",
-    description: "ksadmlsakdmalskdasdklas",
-    img: "",
-    id: 3,
   },
 ];
 
