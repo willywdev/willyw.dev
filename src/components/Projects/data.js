a7c848d089f420113fb3d9c511d8192df2c46c58
Wilhelm Werwein <werwein.dev@gmail.com>
2023-09-01 17:56:07 +0200

    cleanup code + create components

---
+++ src/components/Projects/data.js
@@ -0,0 +1,18 @@
+const projects = [
+  {
+    id: 1,
+    imgSrc: "./images/linkster.png",
+    title: "Linkster",
+    description: "Lorem Ipsum",
+    link: "",
+  },
+  {
+    id: 2,
+    imgSrc: "./images/linktrim.png",
+    title: "LinkTrim",
+    description: "Lorem Ipsum",
+    link: "",
+  },
+];
+
+export default projects;
