5dfa8c574959bd16ce945f1a86e53337f86cd4a9
Wilhelm Werwein <werwein.dev@gmail.com>
2023-09-08 18:25:20 +0200

    remove Form again

--- .backup/src/components/Footer/Footer.jsx
+++
@@ -1,9 +0,0 @@
-import "./Footer.css";
-
-export default function Footer() {
-  return (
-    <footer className="footer">
-      <p className="footer__text">Created with ❤️ and React</p>
-    </footer>
-  );
-}
