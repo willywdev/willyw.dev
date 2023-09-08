7b69c2e99c55fc8b1776be2b56dcaaffcb10a8c9
Wilhelm Werwein <werwein.dev@gmail.com>
2023-09-08 18:05:23 +0200

    remove vercel package

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,7 @@
-import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
-
-import vercel from "@astrojs/vercel/serverless";
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react()],
-  output: "server",
-  adapter: vercel()
 });
