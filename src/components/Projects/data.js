89c5adab60285d54d7e2f8743d6e1b0afc54bffa
willywdev <werwein.dev@gmail.com>
2023-11-11 16:11:48 +0100

    add smartsaver project + image

--- src/components/Projects/data.js
+++ src/components/Projects/data.js
@@ -1,15 +1,23 @@
 const projectData = [
   {
     id: 1,
-    imgSrc: "./images/linkster.png",
-    title: "Linkster",
+    imgSrc: "./images/smartsaver.png",
+    title: "SmartSaver",
     description:
-      "A small Linktree Clone with my personal links. Click here if you want to see more projects of mine.",
-    link: "https://willywdev.github.io/linkster/",
-    repo: "https://github.com/willywdev/linkster",
+      "A web-based expense tracker for better control of income and expenses. Helps with budgeting and provides clear financial overviews for easy financial planning.",
+    link: "https://capstone-smart-saver.vercel.app/",
+    repo: "https://github.com/hdkuzmahashmi/capstone-smartSaver",
   },
   {
     id: 2,
+    imgSrc: "./images/linktrim.png",
+    title: "LinkTrim",
+    description: "Shorten your links with ease! Using URL Shortener API ",
+    link: "https://linktrim.willydev.de/",
+    repo: "https://github.com/willywdev/linktrim",
+  },
+  {
+    id: 3,
     imgSrc: "./images/linktrim.png",
     title: "LinkTrim",
     description: "Shorten your links with ease! Using URL Shortener API ",
