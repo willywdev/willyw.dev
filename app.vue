77f9b73747bc1d7c130b8a1d22c4cb53a8efa2d6
willywdev <werwein.dev@gmail.com>
2023-12-09 13:59:37 +0100

    Add SEO meta tags and favicon, and integrate SpeedInsights component

--- app.vue
+++ app.vue
@@ -1,8 +1,35 @@
 <script setup>
 import Header from "./components/Header.vue";
+import { SpeedInsights } from "@vercel/speed-insights/nuxt";
+
+useSeoMeta({
+  description: "[description]",
+  ogTitle: "[og:title]",
+  ogDescription: "[og:description]",
+  ogImage: "[og:image]",
+  ogUrl: "[og:url]",
+  twitterTitle: "[twitter:title]",
+  twitterDescription: "[twitter:description]",
+  twitterImage: "[twitter:image]",
+  twitterCard: "summary",
+});
+
+useHead({
+  htmlAttrs: {
+    lang: "en",
+  },
+  link: [
+    {
+      rel: "icon",
+      type: "image/png",
+      href: "/favicon.png",
+    },
+  ],
+});
 </script>
 
 <template>
+  <SpeedInsights />
   <Header />
 </template>
 
