ad4f38bc5bdc9d20c799e3ecd8dba939aaa512fe
willyw.dev <kontakt@willyw.dev>
2024-03-16 09:42:20 +0100

    Add ImgIsland component and update styling in AppIsland.vue

--- app.vue
+++ app.vue
@@ -1,5 +1,6 @@
 <script setup>
 import AppIsland from "./components/AppIsland.vue";
+import ImgIsland from "./components/ImgIsland.vue";
 </script>
 
 <template>
@@ -7,16 +8,19 @@ import AppIsland from "./components/AppIsland.vue";
     <section class="grid grid-flow-col gap-4">
       <app-island>
         <h1 class="mb-2 text-4xl tracking-wide text-center">Wilhelm Werwein</h1>
-        <p class="text-2xl tracking-wide">
+        <p class="text-xl tracking-wide">
           Processing coffee and ideas into code
         </p>
       </app-island>
-      <app-island bg-color="bg-[#414f8a]" border-color="border-[#5d6db0]">
+      <app-island bg-color="bg-[#2d3a70]" border-color="border-[#5d6db0]">
         <img
           src="/macavatar.webp"
           alt="avatar"
           class="w-32 h-32 rounded-full" />
       </app-island>
+      <img-island
+        ><img src="/map.webp" class="w-full h-full" alt="map"
+      /></img-island>
     </section>
   </main>
 </template>
--- components/ImgIsland.vue
+++ components/ImgIsland.vue
@@ -1,13 +1,11 @@
 <script setup>
 const props = defineProps({
-  bgColor: String,
   borderColor: String,
 });
 
 const classComputed = computed(() => {
-  const bgColor = props.bgColor || "bg-zinc-800";
   const borderColor = props.borderColor || "border-zinc-700";
-  return `${bgColor} ${borderColor} p-6 border shadow-xl rounded-xl tracking-wide flex flex-col items-center justify-center`;
+  return `w-full h-full aspect-square ${borderColor} border shadow-xl rounded-xl tracking-wide flex flex-col items-center justify-center`;
 });
 </script>
 
